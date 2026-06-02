db1e97150adbb63e891c4433a5653b30b524781b
Krissy Morency <krissy.morency@slalom.com>
2026-06-02 09:12:33 -0400

    veutify install

--- LESSON_SIX/src/main.ts
+++ LESSON_SIX/src/main.ts
@@ -1,11 +1,28 @@
 import './assets/main.css'
+import 'vuetify/styles'
+import '@mdi/font/css/materialdesignicons.css'
 
 import { createApp } from 'vue'
+import { createVuetify } from 'vuetify'
+import { aliases, mdi } from 'vuetify/iconsets/mdi'
+
 import App from './App.vue'
 import router from './router'
+
+const vuetify = createVuetify({
+  icons: {
+    defaultSet: 'mdi',
+    aliases,
+    sets: { mdi },
+  },
+  theme: {
+    defaultTheme: 'light',
+  },
+})
 
 const app = createApp(App)
 
 app.use(router)
+app.use(vuetify)
 
 app.mount('#app')
